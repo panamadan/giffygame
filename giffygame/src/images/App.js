34755bede91aab23d6bc699e4913c9352dc6ab99
Juan De La Guardia <delaguardia@outlook.com>
2020-02-18 20:51:09 -0500

    new outline

---
+++ giffygame/src/images/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import imageCard from;
+import Header from;
+import Wrapper from;
+import Scoreboard from;
+import Data from;
+import "./App.cxs";
+
+class App extends Component {
+  state={
+
+  };
+  shuffleCard = array => {
+
+  }
+  imageClick = array => {
+
+  }
+  imageClick = event =>{
+    const currentImage = event.target.all;
+
+    const alreadyClicked = this.state.clickedImages.indexoff(currentImage) > -1;
+    if []{
+
+    };
+    else {
+      this.setState{
+
+      }
+    },
+  {}=>{
+    if[]{
+      alert();
+      this.setState9{
+
+      }
+    }
+  }
+}
+render []{
+  return{
+    <div>
+    <Header/>
+    <Scoreboard/>
+    <Wrapper>
+    <ImageCard>
+    </ImageCard>
+    </Wrapper>
+    
+    </div>
+  }
+}
+export default App;
---
+++ giffygame/src/images/data.js
@@ -0,0 +1,10 @@
+id 
+image 
+const data =[
+    {
+        "id": 1,
+        image: require('./images/v1.jpg')
+    },
+]
+
+export default data
